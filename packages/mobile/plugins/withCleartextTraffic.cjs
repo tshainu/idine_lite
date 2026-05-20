402ce613f38e3e609410788f0fb397f1409faef9
tshainu <tshainu@users.noreply.github.com>
2026-05-20 18:53:43 +0000

    fix: resolve config-plugins from monorepo path in cleartext plugin

--- packages/mobile/plugins/withCleartextTraffic.cjs
+++ packages/mobile/plugins/withCleartextTraffic.cjs
@@ -1,4 +1,11 @@
-const { withAndroidManifest } = require("@expo/config-plugins");
+const path = require("path");
+
+// Resolve @expo/config-plugins from monorepo root
+const configPluginsPath = path.resolve(
+  __dirname,
+  "../../../node_modules/.bun/@expo+config-plugins@54.0.4/node_modules/@expo/config-plugins"
+);
+const { withAndroidManifest } = require(configPluginsPath);
 
 module.exports = function withCleartextTraffic(config) {
   return withAndroidManifest(config, (config) => {
